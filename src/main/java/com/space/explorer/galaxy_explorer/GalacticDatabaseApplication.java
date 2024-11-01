999cce55d09d0160b13a8b4c804ebb805e6f7716
Melo <joaovmelo.17@gmail.com>
2024-11-01 12:20:55 -0300

    Início do projeto e arquivo inicial

---
+++ src/main/java/com/space/explorer/galaxy_explorer/GalacticDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.space.explorer.galaxy_explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space/explorer/galaxy_explorer/GalacticDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.explorer.galaxy_explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
